12d433db94655703953fed6a0b0686d0edb3750a
fabian parentelli <fabianparentelli007@hotmail.com>
2023-04-09 18:57:28 -0300

    09abrilB

--- src/app.js
+++ src/app.js
@@ -3,13 +3,43 @@ import ProductManager from "./productmanager/productManager.js";
 
 const app = express();
 
-const productManager = new ProductManager('./files/products.json');
+const productManager = new ProductManager('./src/files/products.json');
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/products', async (req, res) => {
-    const products = await productManager.getAll();
-    res.send(products);
+    try {
+        const limit = Number(req.query.limit);
+
+        const products = await productManager.getAll();
+        const limitProduct = products.slice(0, limit);
+
+        if (!limit) {
+            res.send(products);
+        } else {
+            res.send(limitProduct);
+        };
+
+    } catch (error) {
+        console.log(error);
+    };
+});
+
+app.get('/products/:pid', async (req, res) => {
+    try {
+        const ProductId = Number(req.params.pid);
+        const products = await productManager.getAll();
+
+        const product = products.find(prod => prod.id === ProductId);
+        if (product) {
+            res.send(product);
+        } else {
+            res.send(`${ProductId} no coincide con nuestra base de datos`);
+        };
+
+    } catch (error) {
+        console.log(error);
+    };
 });
 
 // console.log(await productManager.getAll());
@@ -17,13 +47,13 @@ app.get('/products', async (req, res) => {
 // console.log(await productManager.getById(2));
 
 const createProduct = async () => {
-    
+
     const obj = {
-        title: "Cafe", 
-        description: 'rico', 
-        price: 200, 
-        thumbnail: 'no hay foto', 
-        code: 500, 
+        title: "Cafe",
+        description: 'rico',
+        price: 200,
+        thumbnail: 'no hay foto',
+        code: 500,
         stock: 50,
     };
 
@@ -35,11 +65,11 @@ const createProduct = async () => {
 const modificaProducto = async () => {
 
     const obj = {
-        title: "manteca", 
-        description: 'rico', 
-        price: 200, 
-        thumbnail: 'no hay foto', 
-        code: 500, 
+        title: "manteca",
+        description: 'rico',
+        price: 200,
+        thumbnail: 'no hay foto',
+        code: 500,
         stock: 50,
     };
     await productManager.updateProduct(4, obj);
